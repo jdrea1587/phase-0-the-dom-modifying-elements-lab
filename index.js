39cf0ad689cbb45e7a5cf13f148808eef3a547b2
Joshua <joshdanielrea@gmail.com>
2021-06-10 11:40:19 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//const element = document.createElement('div'); notes
+//document.body.appendChild(element) notes
+main.remove(); 
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+victory.innerHTML = "<h1>We/Josh are/is the champions my friend</h1>";
+
